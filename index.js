bea0effe173847883fe6c21938a8bdaf6173aeb9
Caleb Engle <calebengle@Calebs-MacBook-Pro-3.local>
2018-03-11 10:59:02 -0400

    simplified my code

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 var play = window.prompt('do you want to play a guessing game? Yes or no?')
 
-if (play === null || play === undefined) {
+if (!play) {
   window.alert('Refresh the page and try again')
 } else {
   play = play.trim().toLowerCase()
-  if (play === 'no') {
-    window.alert('bummer :( refesh the page if you change your mind.')
+  if (play === 'no' || play !== 'yes') {
+    window.alert('bummer :(, you either didn\'t reply with \'yes\' or \'no\', or you just chose \'no\'. Refesh the page if you change your mind.')
   } else if (play === 'yes') {
     var question = window.prompt('Awesome! I\'m thinking of a number between one and three. What is the number?')
     question = question.trim()
     question = parseInt(question)
-    var randomNumber = Math.random()
-    if (randomNumber > 0.66) {
-      randomNumber = 1
-    } else if (randomNumber > 0.33) {
-      randomNumber = 2
-    } else {
-      randomNumber = 3
-    }
+    var randomNumber = parseInt(Math.random() * 3 + 1)
     if (question !== randomNumber) {
       window.alert('Sorry wrong choice. Refresh the page to try again!')
-    } else if (question === randomNumber) {
+    } else {
       var confirmed = window.confirm('Are you sure you want to go with this guess?')
       if (confirmed) {
         window.alert('Congrats, You guessed it!')
